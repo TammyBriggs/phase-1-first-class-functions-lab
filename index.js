2a80161e5a8319cc340393c6932c5370128eb697
TammyBriggs <tamunotonye.briggs@student.moringaschool.com>
2023-03-22 21:31:06 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers) {
+        const newArr = drivers.slice(0, 2)
+        return newArr;
+}
+
+const returnLastTwoDrivers = function (drivers) {
+        const newArr = drivers.slice(-2)
+        return newArr;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (int) {
+        return function(fare) {
+                return fare * int;
+        }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func) {
+        return func(drivers);
+}
